2da560345afe4597e4bcb793b57b2cfeeffc2d4c
Elvar Jonsson <eojons@gmail.com>
2018-04-19 15:27:28 +0000

    test2048 class and simple command line test script

--- 2048-python/test.py
+++ 2048-python/test.py
@@ -1,12 +1,10 @@
-from puzzle import GameGrid
-from random import *
+from test2048 import test2048
+import sys
 
-gamegrid = GameGrid(manual_input = True)
-gamegrid.mainloop()
+man_in = bool(sys.argv[1])
+rnd = bool(sys.argv[2])
+stps = int(sys.argv[3])
+slp = int(sys.argv[4])
 
-for k in range(10):
-    num = randint(0,3)
-    print(num)
-
-
-
+tst = test2048(manual_input=man_in, random=rnd, steps=stps, sleep=slp)
+tst.run()
---
+++ 2048-python/test2048.py
@@ -0,0 +1,33 @@
+from puzzle import GameGrid
+from random import *
+from logic import game_state
+import time
+
+# Event for manual inp
+class event_rn:
+    def __init__(self):
+        self.char = ''
+
+class test2048:
+    def __init__(self, manual_input = True, random=True, steps=10, sleep=0):
+        self.gamegrid = GameGrid(manual_input=manual_input)
+        #self.gamegrid.mainloop()
+        self.random = random
+        self.steps = steps
+        self.sleep = sleep
+
+    def run(self):
+        # Eitthad
+        if self.random:
+            self.run_random()
+    
+    def run_random(self):
+        for k in range(self.steps):
+            num = randint(0,3)
+            event_rn.char = chr(num)
+            self.gamegrid.key_down(event_rn)
+            if game_state(self.gamegrid.matrix) == 'win' \
+            or game_state(self.gamegrid.matrix) == 'lose':
+                time.sleep(1)
+                return
+            time.sleep(self.sleep)
