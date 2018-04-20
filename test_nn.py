6145913ce37c7b3416b6c94f78b4087fa77fcebd
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-04-20 20:48:35 +0000

    Cleaned up the test function a bit.

--- test_nn.py
+++ test_nn.py
@@ -1,4 +1,5 @@
 from nn import NeuralNetwork
+from random import randint
 
 # TEST CODE
 
@@ -7,8 +8,28 @@ nn.input_layer[0].set_value(0)
 nn.input_layer[1].set_value(2)
 
 nn.update_all_layers()
-print nn.output_layer
+print nn.input_layer, nn.output_layer
 
-nn.input_layer.set_values([1.0, 1.5])
-nn.update_all_layers()
-print nn.output_layer
+for k in range(10):
+    in1 = nn.input_layer.get_values()[0]
+    in2 = nn.input_layer.get_values()[1]
+    new_in = [in1, in2]
+    if in1 == in2:
+        if randint(0, 1) == 0:
+            new_in = [in1 + in2, 0]
+        else:
+            new_in = [0, in1 + in2]
+    elif in1 == 0:
+        new_in[0] = 2
+    elif in2 == 0:
+        new_in[1] = 2
+    elif in1 < in2:
+        new_in[0] *=2
+    elif in1 > in2:
+        new_in[1] *=2
+        
+    nn.input_layer.set_values(new_in)
+    nn.update_all_layers()
+    print nn.input_layer, nn.output_layer
+
+
