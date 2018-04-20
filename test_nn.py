39cc82421b0d36a471a372f2a53b707f6ccee8b4
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-04-20 20:17:01 +0000

    Changed the output to the output layer.

--- test_nn.py
+++ test_nn.py
@@ -7,8 +7,8 @@ nn.input_layer[0].set_value(0)
 nn.input_layer[1].set_value(2)
 
 nn.update_all_layers()
-print nn.hidden_layers
+print nn.output_layer
 
 nn.input_layer.set_values([1.0, 1.5])
 nn.update_all_layers()
-print nn.hidden_layers
+print nn.output_layer
