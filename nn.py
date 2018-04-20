76e7dd3540670ef17f0004b9e79336780afe4137
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-04-20 19:07:54 +0000

    Changed the dummy input value to be closer to 0 for debugging purposes.

--- nn.py
+++ nn.py
@@ -187,7 +187,7 @@ class NeuralNetwork():
 
 nn = NeuralNetwork([4, 2], 2, 1)
 nn.input_layer[0].set_value(0)
-nn.input_layer[1].set_value(32)
+nn.input_layer[1].set_value(2)
 
 nn.update_all_layers()
 
